cd03c0d44cedef01e2ad85ae33c5e87ee8a40aa3
Alexis O'Neill <alexisoneill@Alexiss-MacBook-Pro.local>
2019-11-09 14:04:05 -0800

    Only use the right port

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/check_answers", function(req, res) {
 });
 
 // running server
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
 });
 
